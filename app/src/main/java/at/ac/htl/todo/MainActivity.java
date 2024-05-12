dc7bc76574861585fbff0a13f269be51c71f8d95
jonas.pamminger <jonas.pamminger@outlook.de>
2024-05-12 06:35:46 +0200

    added rest client

--- app/src/main/java/at/ac/htl/todo/MainActivity.java
+++ app/src/main/java/at/ac/htl/todo/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import androidx.activity.ComponentActivity;
 import javax.inject.Inject;
 
+import at.ac.htl.todo.model.TodoService;
 import at.ac.htl.todo.ui.layout.MainView;
 import at.ac.htl.todo.util.Config;
 import dagger.hilt.android.AndroidEntryPoint;
@@ -18,6 +19,9 @@ public class MainActivity extends ComponentActivity {
     @Inject
     MainView mainView;
 
+    @Inject
+    TodoService todoService;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +29,6 @@ public class MainActivity extends ComponentActivity {
         var base_url = Config.getProperty("json.placeholder.baseurl");
         Log.i(TAG, "onCreate: " + base_url);
         mainView.buildContent(this);
+        todoService.getAll();
     }
 }
--- app/src/main/java/at/ac/htl/todo/model/Model.java
+++ app/src/main/java/at/ac/htl/todo/model/Model.java
@@ -1,12 +1,8 @@
 package at.ac.htl.todo.model;
-import java.util.List;
+
+import javax.inject.Inject;
 
 public class Model {
-
-    public Todo[] todos = {
-            new Todo(1L, 1L, "Buy milk", true),
-            new Todo(2L, 2L, "Buy eggs", false),
-            new Todo(2L, 3L, "Buy bread", false)
-    };
+    public Todo[] todos = new Todo[0];
 
 }
